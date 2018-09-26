4d8b9e3104a4c4255cae9105caa6021e32b99306
DevKiHyun <gunatenam@naver.com>
2018-09-27 00:07:58 +0900

    Update main.py

--- RDN/main.py
+++ RDN/main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     parser.add_argument('--beta_2', type=float, default=0.999, help='-')
     parser.add_argument('--epsilon', type=float, default=1e-08, help='-')
     parser.add_argument('--training_epoch', type=int, default=200, help='-')
-    parser.add_argument('--batch_size', type=int, default=128, help='-')
+    parser.add_argument('--batch_size', type=int, default=16, help='-')
     parser.add_argument('--test_batch_size', type=int, default=5, help='-')
     parser.add_argument('--n_channel', type=int, default=3, help='-')
     args, unknown = parser.parse_known_args()
